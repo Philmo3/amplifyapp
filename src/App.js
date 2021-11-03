31fb8f99b0d85e9c61a9f23f74cc85582c9036b4
Philippe Brunet <philippe.brunet@logient.com>
2021-11-03 14:43:30 -0400

    add api end get endpoint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify'
 
 function App() {
+
+  const [people , setPeople] = useState([]);
+  
+  useEffect( async () => {
+    async function fetchPeople(){
+      const data = await API.get('peopleapi', '/people')
+      console.log(data);
+      setPeople(data.people);
+    }
+    fetchPeople();
+  })
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
-      <AmplifySignOut />
+      {
+        people.map( person => (
+          <div>
+          name : {person.name} & age: {person.age}
+          </div>
+        ))
+      }
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
